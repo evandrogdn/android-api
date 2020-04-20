36d0e147b5ba5bd4755faaf17b94a2527984b3d8
evandro_gdn <evandrogardolinn@gmail.com>
2020-04-20 00:12:53 -0300

    [ADD] #set - Adicionado set dos dados no form durante o update

--- app/src/main/java/com/example/android_api/FormActivity.java
+++ app/src/main/java/com/example/android_api/FormActivity.java
@@ -8,13 +8,29 @@ import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.android_api.helper.FormHelper;
+import com.example.android_api.model.Censos;
+import com.example.android_api.model.Dados;
+import com.google.gson.Gson;
+
 public class FormActivity extends AppCompatActivity {
     private Button button;
+    private Censos censoToUpdate;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            Censos censos = (Censos) getIntent().getSerializableExtra("censo_selecionado");
+            if (censos != null) {
+                censoToUpdate = censos;
+                Dados dados = (new Gson()).fromJson(censos.getDados(), Dados.class);
+                (new FormHelper(FormActivity.this)).setDadosIntoForm(dados);
+            }
+        }
 
         button = findViewById(R.id.botao);
         addListeners();
